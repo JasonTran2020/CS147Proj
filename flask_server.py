0085ba501a6400d0f06d59d201d7977236c743a0
Oscar Tran <otran2@uci.edu>
2024-05-31 16:37:15 -0700

    Setup flask

--- flask_server.py
+++ flask_server.py
@@ -12,7 +12,7 @@ cur_humidity = None
 @app.route("/")
 def hello():
     print(request.args.get("var"))
-    return "We received value: " + str(request.args.get("var"))
+    return render_template("home.html")
 
 
 @app.route("/post", methods=['POST'])
@@ -20,12 +20,12 @@ def set_stat():
     if request.method == 'POST':
         data_json = request.json
         audio, motion, id = data_json.get('audio'), data_json.get('motion'),data_json.get('id')
-        print("Audio data is:" + data_json.get('audio'))
-        print("Motion data is:" + data_json.get('motion'))
-        print("id is:" + data_json.get('id'))
+        print("Audio data is:" + str(data_json.get('audio')))
+        print("Motion data is:" + str(data_json.get('motion')))
+        print("id is:" + str(data_json.get('id')))
         print(data_json)
         insert_recording(id,audio,motion)
-        
+        return "Posted"
 
 
 @app.route("/get")

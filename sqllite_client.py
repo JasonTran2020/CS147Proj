8903f0286bd1570db720849b54c11430bd0d19c6
Oscar Tran <otran2@uci.edu>
2024-06-03 02:32:20 -0700

    Added get_all_within_day()

--- sqllite_client.py
+++ sqllite_client.py
@@ -1,10 +1,12 @@
 import sqlite3
 import datetime
+from datetime import datetime, timedelta
+
 def insert_recording(device_id:int, audio:int, motion:int):
     """Assume that the data has already been processed into the form that is supposed to be saved"""
     with sqlite3.connect("database.db") as connection:
         cursor = connection.cursor()
-        cursor.execute("INSERT INTO data_records VALUES(?,?,?,?)",(device_id,datetime.datetime.now(tz=datetime.UTC),audio,motion))
+        cursor.execute("INSERT INTO data_records VALUES(?,?,?,?)",(device_id,datetime.datetime.now(tz=datetime.timezone.utc),audio,motion))
         connection.commit()
         cursor.close()
 
@@ -19,6 +21,24 @@ def get_all_device_records(device_id:int):
             result.append(py_dict)
         cursor.close()
         return result
+
+#Return result list and a value indicating minimum audio
+def get_all_within_day(device_id:int, date:datetime):
+    #Format to convert string to datetime, and the minimum date time we will consider (24 hour period from the given datetime)
+    date_format = '%Y-%m-%d %H:%M:%S.%f%z'
+    startConsider = date - timedelta(hours = 24)
+    with sqlite3.connect("database.db") as connection:
+        cursor = connection.cursor()
+        cursor.execute("SELECT * FROM data_records WHERE id = ?", (device_id,))
+        
+        result = []
+        for entry in cursor.fetchall():
+            entryDate = datetime.strptime(entry[1], date_format)
+            if startConsider <= entryDate <= date:
+                py_dict = {'datetime':entry[1],'audio':entry[2],'motion':entry[3]}
+                result.append(py_dict)
+        cursor.close()
+        return result
     
 def initialize_tables():
     with sqlite3.connect("database.db") as connection:
@@ -30,5 +50,6 @@ def initialize_tables():
     
     
 if __name__ == '__main__':
+    initialize_tables()
     insert_recording(1,2,3)
     get_all_device_records(1)
